8f791e20a1d2e3985039197de645ec3031212469
Mdsaad-07 <saadmohammedg@gmail.com>
2025-08-29 00:39:11 +0530

    Enable CORS for Netlify frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,            # [REQUIRED] your Netlify URL only!
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
